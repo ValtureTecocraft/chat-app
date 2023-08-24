38da9227481fdfaa9644a549a8032ab6fa647db3
ValtureTecocraft <smit.valture@tecocraft.com>
2023-08-24 16:38:00 +0530

    init

---
+++ tailwind.config.js
@@ -0,0 +1,15 @@
+module.exports = {
+  content: ["./src/**/*.{js,jsx,ts,tsx}"],
+  important: "#root",
+  theme: {
+    extend: {
+      boxShadow: {
+        "3xl": "0 0 35px 10px #87fbb0",
+      },
+      fontFamily: {
+        Josefin: ["Josefin Sans", "sans-serif"],
+      },
+    },
+  },
+  plugins: [],
+};
---
+++ vite.config.ts
@@ -0,0 +1,19 @@
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react";
+
+// https://vitejs.dev/config/
+export default defineConfig({
+  plugins: [react()],
+  build: {
+    chunkSizeWarningLimit: 1000 * 1024,
+    rollupOptions: {
+      output: {
+        manualChunks: {
+          // Define manual chunks here
+          // For example:
+          myChunk: ["lodash", "axios"],
+        },
+      },
+    },
+  },
+});
