1ee3ad874060874f294cc6b471ee2c492203fe91
ValtureTecocraft <smit.valture@tecocraft.com>
2023-08-31 11:17:19 +0530

    scroll behaviour

--- src/components/Message.tsx
+++ src/components/Message.tsx
@@ -3,91 +3,91 @@ import { AuthContext } from "../context/AuthContext";
 import { ChatContext } from "../context/ChatContext";
 
 export const Message = ({
-	message,
-	imgClick,
+  message,
+  imgClick,
 }: {
-	message: any;
-	imgClick: any;
+  message: any;
+  imgClick: any;
 }) => {
-	// console.log(message);
+  // console.log(message);
 
-	const currentUser = useContext(AuthContext);
-	const { data } = useContext(ChatContext);
+  const currentUser = useContext(AuthContext);
+  const { data } = useContext(ChatContext);
 
-	// console.log(message.date);
+  // console.log(message.date);
 
-	const scrollRef = useRef<HTMLDivElement>(null);
+  const scrollRef = useRef<HTMLDivElement>(null);
 
-	useEffect(() => {
-		scrollRef.current?.scrollIntoView({ behavior: "smooth" });
-	}, [message]);
+  useEffect(() => {
+    scrollRef.current?.scrollIntoView();
+  }, [message]);
 
-	const convertTimestampToTime = (timestamp: any) => {
-		const currentTime: any = new Date();
-		const targetTime: any = new Date(timestamp.seconds * 1000); // Convert seconds to milliseconds
+  const convertTimestampToTime = (timestamp: any) => {
+    const currentTime: any = new Date();
+    const targetTime: any = new Date(timestamp.seconds * 1000); // Convert seconds to milliseconds
 
-		// Calculate the time difference in seconds
-		const timeDiffInSeconds = Math.floor((currentTime - targetTime) / 1000);
+    // Calculate the time difference in seconds
+    const timeDiffInSeconds = Math.floor((currentTime - targetTime) / 1000);
 
-		if (timeDiffInSeconds < 60) {
-			return "just now";
-		}
+    if (timeDiffInSeconds < 60) {
+      return "just now";
+    }
 
-		const hours = targetTime.getHours();
-		const minutes = targetTime.getMinutes();
-		const ampm = hours >= 12 ? "PM" : "AM";
-		const formattedHours = hours % 12 === 0 ? 12 : hours % 12;
-		const formattedMinutes = String(minutes).padStart(2, "0");
-		return `${formattedHours}:${formattedMinutes} ${ampm}`;
-	};
+    const hours = targetTime.getHours();
+    const minutes = targetTime.getMinutes();
+    const ampm = hours >= 12 ? "PM" : "AM";
+    const formattedHours = hours % 12 === 0 ? 12 : hours % 12;
+    const formattedMinutes = String(minutes).padStart(2, "0");
+    return `${formattedHours}:${formattedMinutes} ${ampm}`;
+  };
 
-	return (
-		<div
-			ref={scrollRef}
-			className={`${
-				message.senderId === currentUser.uid ? "flex-row-reverse" : "flex-row"
-			} flex gap-4`}
-		>
-			<div className="min-w-max">
-				<img
-					className="w-8 h-8 rounded-full object-cover"
-					src={
-						message.senderId === currentUser.uid
-							? currentUser.photoURL
-							: data.user.photoURL
-					}
-					alt="profile"
-				/>
-				<span className="font-light text-xs text-white">
-					{convertTimestampToTime(message.date)}
-				</span>
-			</div>
+  return (
+    <div
+      ref={scrollRef}
+      className={`${
+        message.senderId === currentUser.uid ? "flex-row-reverse" : "flex-row"
+      } flex gap-4`}
+    >
+      <div className="min-w-max">
+        <img
+          className="w-8 h-8 rounded-full object-cover"
+          src={
+            message.senderId === currentUser.uid
+              ? currentUser.photoURL
+              : data.user.photoURL
+          }
+          alt="profile"
+        />
+        <span className="font-light text-xs text-white">
+          {convertTimestampToTime(message.date)}
+        </span>
+      </div>
 
-			<div
-				className={`${
-					message.senderId === currentUser.uid ? "items-end" : ""
-				} gap-2 flex flex-col w-full`}
-			>
-				{message.text && (
-					<p
-						className={`${
-							message.senderId === currentUser.uid
-								? "bg-[#414857] text-white rounded-[10px_0px_10px_10px] bubbleOwner"
-								: "bg-white rounded-[0px_10px_10px_10px] bubble"
-						} max-w-max p-2 text-sm px-4`}
-					>
-						{message.text}
-					</p>
-				)}
-				{message.img && (
-					<img
-						onClick={() => imgClick(message.img)}
-						className="max-w-[25%] w-full rounded-md cursor-pointer"
-						src={message.img}
-						alt=""
-					/>
-				)}
-			</div>
-		</div>
-	);
+      <div
+        className={`${
+          message.senderId === currentUser.uid ? "items-end" : ""
+        } gap-2 flex flex-col w-full`}
+      >
+        {message.text && (
+          <p
+            className={`${
+              message.senderId === currentUser.uid
+                ? "bg-[#414857] text-white rounded-[10px_0px_10px_10px] bubbleOwner"
+                : "bg-white rounded-[0px_10px_10px_10px] bubble"
+            } max-w-max p-2 text-sm px-4`}
+          >
+            {message.text}
+          </p>
+        )}
+        {message.img && (
+          <img
+            onClick={() => imgClick(message.img)}
+            className="max-w-[25%] w-full rounded-md cursor-pointer"
+            src={message.img}
+            alt=""
+          />
+        )}
+      </div>
+    </div>
+  );
 };
